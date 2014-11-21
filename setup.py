abe7eb49dcf72b65f2ea0aa57b14fb8d9217dec4
Jonathan Tushman <jtushman@pipewave.com>
2014-11-20 19:33:42 -0500

    bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_packages():
 
 setup(
     name='monarch',
-    version='0.1.3',
+    version='0.1.5',
     description='The un-migration migration tool',
     url='http://github.com/jtushman/monarch',
     author='Jonathan Tushman',
